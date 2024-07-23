c94db8704fd4a8dad86acbf0a35860e21f125ebd
Edison <96665518+EdisonAlushaj@users.noreply.github.com>
2024-07-23 18:42:01 +0200

    Back-End Files
    Lidhja me Database dhe krijimi i backend files

---
+++ CarShowroom-App/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarShowroom_App.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
